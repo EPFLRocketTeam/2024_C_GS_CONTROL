35fa4f1cc2a95595adc1695ed9e3209e4ef2066e
Maxime Rochat <rochat.max@outlook.com>
2025-01-06 17:43:26 +0100

    fixed indexing

--- start_tmux.sh
+++ start_tmux.sh
@@ -1,3 +1,5 @@
+
+
 #!/bin/bash
 
 # Path to log files
@@ -15,7 +17,7 @@ tmux new-session -d -s $SESSION_NAME -n $WINDOW_NAME
 
 # Create bottom pane
 tmux split-window -v -t $SESSION_NAME
-tmux select-pane -t 1
+tmux select-pane -t 0
 
 # Pane 1: Tail INFO_LOG
 tmux send-keys -t $SESSION_NAME "tail -f -n 20 $LOG_FILE | sed -n '/\[INFO\]/,/,$/p'" C-m
@@ -29,14 +31,14 @@ tmux send-keys -t $SESSION_NAME "tail -f -n 20 $LOG_FILE | sed -n '/\[ERROR\]/,/
 # Make the bottom pane span the entire width
 # tmux resize-pane -t 2 -y 10 # Adjust height as needed
 
-tmux select-pane -t 3
+tmux select-pane -t 2
 # Run any command in the bottom pane (or leave it for manual use)
-tmux send-keys -t 3 'cd build' C-m
+tmux send-keys -t 2 'cd build' C-m
 
 tmux split-window -h -t $SESSION_NAME
-tmux send-keys -t 4 'cd build' C-m
+tmux send-keys -t 3 'cd build' C-m
 # Focus back on the top-left pane
-tmux select-pane -t 3
+tmux select-pane -t 2
 # Attach to the tmux session
 tmux attach-session -t $SESSION_NAME
 
